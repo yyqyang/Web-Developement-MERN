f625bd48f1d0b711bd4772e1e4cb35cba5184001
yyqyang <yyqyang59@hotmail.com>
2021-05-21 14:10:11 -0700

    Created IssueList class and relative classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-class HelloWorld extends React.Component {
+class IssueFilter extends React.Component {
     render() {
-        const continents = ['Africa','Asia','Australia','Europe'];
-        const helloContinents = Array.from(continents, c => `Hello ${c}`);
-        const message = helloContinents.join(' ')
         return (
-            <div title="Outer div">
-                <h1>{message}</h1>
-            </div>
+            <div>This is a placeholder for the issue filter.</div>
         );
     }
 }
 
-const element = <HelloWorld />;
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue filter.</div>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue filter.</div>
+        );
+    }
+}
+
+class IssueList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </React.Fragment>
+        );
+    }
+}
+const element = <IssueList />;
 ReactDOM.render(element, document.getElementById('content'));
