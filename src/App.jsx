4192dd293b117b1390e974d35745956997511c29
yyqyang <yyqyang59@hotmail.com>
2021-05-21 14:00:45 -0700

    Created HelloWorld Class

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-const continents = ['Africa','Asia','Australia','Europe'];
-const helloContinents = Array.from(continents, c => `Hello ${c}`);
-const message = helloContinents.join(' ')
-const element = (
-    <div title="Outer div">
-        <h1 style={{ color: 'red' }}>{message}</h1>
-    </div>
-);
+class HelloWorld extends React.Component {
+    render() {
+        const continents = ['Africa','Asia','Australia','Europe'];
+        const helloContinents = Array.from(continents, c => `Hello ${c}`);
+        const message = helloContinents.join(' ')
+        return (
+            <div title="Outer div">
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+}
+
+const element = <HelloWorld />;
 ReactDOM.render(element, document.getElementById('content'));
