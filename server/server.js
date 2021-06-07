76c8b5a26e023b92f90af0cfe01a7c47937cc5d9
yyqyang <yyqyang59@hotmail.com>
2021-06-07 14:17:16 -0700

    Set up the GraphQL Playground

--- server/server.js
+++ server/server.js
@@ -1,6 +1,45 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.static('public'));
+// app.listen(3000, function() {
+//     console.log('App started on port 3000');
+// });
 const express = require('express');
+
+const { ApolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+const typeDefs = `
+    type Query {
+        about: String!
+    }
+    type Mutation {
+        setAboutMessage(message: String!): String
+    }
+    `;
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
 const app = express();
 app.use(express.static('public'));
-app.listen(3000, function() {
+server.applyMiddleware({ app, path: '/graphql' });
+app.listen(3000, function () {
     console.log('App started on port 3000');
 });
