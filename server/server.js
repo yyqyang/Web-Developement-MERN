8e91b32989b62d5f0f91c71ee8598e66359ef31a
yyqyang <yyqyang59@hotmail.com>
2021-06-07 15:11:25 -0700

    Custom Scalar Types

--- server/server.js
+++ server/server.js
@@ -7,20 +7,48 @@
 const fs = require('fs');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { GraphQLScalarType } = require('graphql');
+
 
 let aboutMessage = "Issue Tracker API v1.0";
+
+const issuesDB = [
+    {
+        id: 1, status: 'New', owner: 'Ravan', effort: 5,
+        created: new Date('2019-01-15'), due: undefined,
+        title: 'Error in console when clicking Add',
+    },
+    {
+        id:2, status: 'Assigned', owner: 'Eddie', effort: 14,
+        created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+        title: 'Missing bottom border on panel',
+    },
+];
+
+const GraphQLDate = new GraphQLScalarType({
+    name: 'GraphQLDate',
+    description: 'A Date() type in GraphQL as a scalar',
+    serialize(value) {
+        return value.toISOString();
+    },
+});
 
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
     setAboutMessage,
   },
+  GraphQLDate,
 };
 
 function setAboutMessage(_, { message }) {
   return aboutMessage = message;
+}
+function issueList() { 
+    return issuesDB;
 }
 
 const server = new ApolloServer({
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-const initialIssues = [{
-    id: 1, status: 'New', owner: 'Ravan', effort: 5, 
-    created: new Date('2018-08-15'), due: undefined,
-    title: 'Error in console when clicking Add',
-},
-{
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, 
-    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-    title: 'Missing bottom border on panel',
-},
-];
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+function jsonDateReviver(key, value) {
+    if (dateRegex.test(value)) return new Date(value);
+    return value;
+}
 
 
 class IssueFilter extends React.Component {
@@ -81,7 +75,7 @@ function IssueRow (props) {
           <td>{issue.owner}</td>
           <td>{issue.created.toDateString()}</td>
           <td>{issue.effort}</td>
-          <td>{issue.due ? issue.due.toDateString() : ''}</td>
+          <td>{issue.due ? issue.due.toDateString() : ' '}</td>
           <td>{issue.title}</td>
       </tr>
     );
@@ -108,11 +102,22 @@ class IssueList extends React.Component {
         this.loadData();
     }
 
-    loadData() {
-        setTimeout(() => {
-            this.setState({ issues: initialIssues });
-        }, 500);
-    }
+    async loadData() {
+        const query = `query {
+          issueList {
+            id title status owner
+            created effort due
+          }
+        }`;
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ query })
+          });
+          const body = await response.text();
+          const result = JSON.parse(body, jsonDateReviver);
+          this.setState({ issues: result.data.issueList });
+        }
 
     render() {
         return (
