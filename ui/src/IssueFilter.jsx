06d03d6c46a6ac35deca55a6d09b97d35acd09b2
yyqyang <yyqyang59@hotmail.com>
2021-06-30 12:44:45 -0700

    Transform and bundle

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,12 @@
+  
+/* globals React */
+/* eslint "react/prefer-stateless-function": "off" */
+
+export default class IssueFilter extends React.Component {
+    render() {
+      return (
+        <div>This is a placeholder for the issue filter.</div>
+      );
+    }
+  }
+  
