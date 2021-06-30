c38c198d45797178e67a5362c724ece955a9e0b3
yyqyang <yyqyang59@hotmail.com>
2021-06-30 16:58:09 -0700

    simple routing

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,10 +2,14 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
-import IssueList from './IssueList.jsx';
-
-const element = <IssueList />;
+import Page from './Page.jsx';
+const element = (
+  <Router>
+    <Page />
+  </Router>
+);
 
 ReactDOM.render(element, document.getElementById('contents'));
 
