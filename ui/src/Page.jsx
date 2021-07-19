2a1c78b21a26e6aa78ff925edbb64f1d2d802e22
yyqyang <yyqyang59@hotmail.com>
2021-07-19 14:46:12 -0700

    React context

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,3 +1,4 @@
+  
 import React from 'react';
 import {
   Navbar, Nav, NavItem, NavDropdown,
@@ -10,6 +11,7 @@ import Contents from './Contents.jsx';
 import IssueAddNavItem from './IssueAddNavItem.jsx';
 import SignInNavItem from './SignInNavItem.jsx';
 import Search from './Search.jsx';
+import UserContext from './UserContext.js';
 
 function NavBar({ user, onUserChange }) {
   return (
@@ -94,7 +96,9 @@ export default class Page extends React.Component {
       <div>
         <NavBar user={user} onUserChange={this.onUserChange} />
         <Grid fluid>
-          <Contents />
+          <UserContext.Provider value={user}>
+            <Contents />
+          </UserContext.Provider>
         </Grid>
         <Footer />
       </div>
