9652e28ec30e47fce53617a7f7da7c21b14d7812
yyqyang <yyqyang59@hotmail.com>
2021-07-19 15:07:40 -0700

    change host address

--- api/auth.js
+++ api/auth.js
@@ -60,8 +60,7 @@ routes.post('/signin', async (req, res) => {
   };
 
   const token = jwt.sign(credentials, JWT_SECRET);
-  res.cookie('jwt', token, { httpOnly: true });
-
+  res.cookie('jwt', token, { httpOnly: true, domain: process.env.COOKIE_DOMAIN });
   res.json(credentials);
 });
 
@@ -85,4 +84,10 @@ function mustBeSignedIn(resolver) {
   };
 }
 
-module.exports = { routes, getUser, mustBeSignedIn };
+function resolveUser(_, args, { user }) {
+    return user;
+  }
+  
+  module.exports = {
+    routes, getUser, mustBeSignedIn, resolveUser,
+  };
--- ui/server/template.js
+++ ui/server/template.js
@@ -1,6 +1,6 @@
 import serialize from 'serialize-javascript';
 
-export default function template(body, data) {
+export default function template(body, initialData, userData) {
   return `<!DOCTYPE HTML>
 <html>
 <head>
@@ -8,9 +8,7 @@ export default function template(body, data) {
   <title>Pro MERN Stack</title>
   <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" >
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
   <script src="https://apis.google.com/js/api:client.js"></script>
-  
   <style>
     table.table-hover tr {cursor: pointer;}
     .panel-title a {display: block; width: 100%; cursor: pointer;}
@@ -19,7 +17,10 @@ export default function template(body, data) {
 <body>
   <!-- Page generated from template. -->
   <div id="contents">${body}</div>
-  <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+  <script>
+    window.__INITIAL_DATA__ = ${serialize(initialData)}
+    window.__USER_DATA__ = ${serialize(userData)}
+  </script>
   <script src="/env.js"></script>
   <script src="/vendor.bundle.js"></script>
   <script src="/app.bundle.js"></script>
